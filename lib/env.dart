2a306fe6ce33dccfe3a9ea76dd7ccf8c46df0d86
ibrahim <ibrahim12212709@gmail.com>
2024-10-22 21:16:03 +0300

    Initial commit

---
+++ lib/env.dart
@@ -0,0 +1,587 @@
+// ignore_for_file: prefer_single_quotes, lines_longer_than_80_chars final
+Map<String, dynamic> environment = {
+  "appConfig": "lib/config/config_en.json",
+  "serverConfig": {
+    "url": "https://toysbox.store",
+    "type": "woo",
+    "consumerKey": "ck_694789857017fa25ec9d2045d3c68ea3925a4281",
+    "consumerSecret": "cs_25ca772ffa84c41aabdc17c5535bbe0abe121ff4"
+  },
+  "defaultDarkTheme": false,
+  "enableRemoteConfigFirebase": false,
+  "enableFirebaseAnalytics": false,
+  "enableFacebookAppEvents": false,
+  "webProxy": "",
+  "maxTextScale": null,
+  "loginSMSConstants": {
+    "nameDefault": "United States",
+    "dialCodeDefault": "+1",
+    "countryCodeDefault": "US"
+  },
+  "phoneNumberConfig": {
+    "enable": true,
+    "countryCodeDefault": "EG",
+    "dialCodeDefault": "+20",
+    "useInternationalFormat": true,
+    "showCountryFlag": true,
+    "customCountryList": [],
+    "formatInput": false,
+    "selectorType": "BOTTOM_SHEET",
+    "selectorFlagAsPrefixIcon": true
+  },
+  "appRatingConfig": {
+    "minDays": 7,
+    "minLaunches": 10,
+    "remindDays": 7,
+    "remindLaunches": 10,
+    "googlePlayIdentifier": "",
+    "appStoreIdentifier": "",
+    "showOnOpen": false
+  },
+  "advanceConfig": {
+    "DefaultLanguage": "en",
+    "DetailedBlogLayout": "halfSizeImageType",
+    "EnablePointReward": false,
+    "hideOutOfStock": false,
+    "HideEmptyTags": false,
+    "HideEmptyCategories": true,
+    "EnableRating": true,
+    "hideEmptyRating": true,
+    "EnableCart": true,
+    "ShowBottomCornerCart": true,
+    "EnableSkuSearch": true,
+    "showStockStatus": true,
+    "GridCount": 3,
+    "isCaching": false,
+    "kIsResizeImage": false,
+    "httpCache": true,
+    "Currencies": [
+      {
+        "symbol": "EGP",
+        "decimalDigits": 2,
+        "symbolBeforeTheNumber": false,
+        "currency": "EGP",
+        "currencyCode": "EGP",
+        "countryCode": ""
+      }
+    ],
+    "DefaultStoreViewCode": "",
+    "EnableAttributesConfigurableProduct": ["color", "size"],
+    "isMultiLanguages": true,
+    "EnableApprovedReview": false,
+    "EnableSyncCartFromWebsite": false,
+    "EnableSyncCartToWebsite": false,
+    "EnableFirebase": false,
+    "RatioProductImage": 1.2,
+    "EnableCouponCode": true,
+    "ShowCouponList": true,
+    "ShowAllCoupons": true,
+    "ShowExpiredCoupons": true,
+    "AlwaysShowTabBar": false,
+    "PrivacyPoliciesPageUrlOrId": "",
+    "AboutUSPageUrl": "https://toysbox-eg.com/index.php/about-us/",
+    "FAQPageUrl": "https://toysbox.store/faq/",
+    "SupportPageUrl": "https://toysbox-eg.com/index.php/contact-us//",
+    "DownloadPageUrl": "https://toysbox-eg.com/",
+    "SocialConnectUrl": [
+
+      {
+        "url": "https://www.facebook.com/toysbox.eg?mibextid=LQQJ4d",
+        "icon": "assets/icons/brands/facebook.svg",
+        "name": "Facebook"
+      },
+
+    ],
+    "AutoDetectLanguage": false,
+    "QueryRadiusDistance": 10,
+    "MinQueryRadiusDistance": 1,
+    "MaxQueryRadiusDistance": 10,
+    "EnableWooCommerceWholesalePrices": false,
+    "IsRequiredSiteSelection": true,
+    "EnableDeliveryDateOnCheckout": false,
+    "EnableNewSMSLogin": true,
+    "EnableBottomAddToCart": true,
+    "inAppWebView": false,
+    "EnableWOOCSCurrencySwitcher": false,
+    "enableProductBackdrop": false,
+    "categoryImageMenu": true,
+    "EnableDigitsMobileLogin": false,
+    "EnableDigitsMobileFirebase": false,
+    "EnableDigitsMobileWhatsApp": false,
+    "WebViewScript": "",
+    "versionCheck": {"enable": false, "iOSAppStoreCountry": "US"},
+    "AjaxSearchURL": "",
+    "AlwaysClearWebViewCache": false,
+    "AlwaysClearWebViewCookie": false,
+    "AlwaysRefreshBlog": false,
+    "OrderNotesWithPrivateNote": true,
+    "OrderNotesLinkSupport": false,
+    "inAppUpdateForAndroid": {"enable": false, "typeUpdate": "flexible"},
+    "categoryConfig": {"deepLevel": 3, "enableLargeCategories": false},
+    "pinnedProductTags": [],
+    "showOpeningStatus": true,
+    "TimeShowToastMessage": 1500,
+    "b2bKingConfig": {
+      "enabled": false,
+      "guestAccessRestriction": "replace_prices_quote"
+    },
+    "enablePWGiftCard": true,
+    "EnableTeraWalletWithdrawal": false,
+    "allowGetDatasByCategoryFilter": false,
+    "gdpr": {
+      "confirmCaptcha": "PERMANENTLY DELETE",
+      "showDeleteAccount": true,
+      "showPrivacyPolicyFirstTime": false
+    },
+    "DefaultCurrency": {
+      "symbol": "£",
+      "decimalDigits": 2,
+      "symbolBeforeTheNumber": true,
+      "currency": "EGP",
+      "currencyCode": "EGP",
+      "countryCode": ""
+    },
+    "PrivacyPoliciesPageId": null,
+    "showRequestNotification": true
+  },
+  "defaultDrawer": {
+    "logo": "assets/images/logo.png",
+    "items": [
+      {"show": true, "type": "home"},
+      {"show": true, "type": "categories"},
+      {"show": true, "type": "cart"},
+      {"show": true, "type": "profile"},
+      {"show": true, "type": "login"},
+      {"show": true, "type": "category"}
+    ],
+    "background": null
+  },
+  "defaultSettings": [
+    "biometrics",
+    "wallet",
+    "chat",
+    "wishlist",
+    "notifications",
+    "language",
+    // "currencies",
+    "darkTheme",
+    "order",
+    "rating",
+    "privacy",
+    "about"
+  ],
+  "loginSetting": {
+    "IsRequiredLogin": false,
+    "showAppleLogin": false,
+    "showFacebook": false,
+    "showSMSLogin": false,
+    "showGoogleLogin": true,
+    "showPhoneNumberWhenRegister": true,
+    "requirePhoneNumberWhenRegister": false,
+    "isResetPasswordSupported": true,
+    "facebookAppId": "430258564493822",
+    "facebookLoginProtocolScheme": "fb430258564493822",
+    "facebookClientToken": "",
+    "smsLoginAsDefault": false,
+    "appleLoginSetting": {
+      "iOSBundleId": "com.inspireui.mstore.flutter",
+      "appleAccountTeamID": "S9RPAM8224"
+    }
+  },
+  "oneSignalKey": {"appID": "", "enable": false},
+  "onBoardingConfig": {
+    "data": [
+      {
+        "desc": "Toys Box brings joy to your doorsteps.",
+        "image": "assets/images/fogg-delivery-1.png",
+        "title": "Welcome to Toys Box"
+      },
+      {
+        "desc": "Discover endless fun with just a tap on your phone. Fast, simple, and playful.",
+        "image": "assets/images/fogg-uploading-1.png",
+        "title": "Explore the World of Toys"
+      },
+      {
+        "desc": "The wait is over, let's start the fun!",
+        "image": "assets/images/fogg-order-completed.png",
+        "title": "Let's Play!"
+      }
+    ],
+    "version": 1,
+    "showLanguage": false,
+    "enableOnBoarding": true,
+    "autoCropImageByDesign": true,
+    "isOnlyShowOnFirstTime": true
+  },
+  "adConfig": {
+    "ads": [
+      {
+        "type": "banner",
+        "iosId": "ca-app-pub-3940256099942544/2934735716",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/6300978111",
+        "showOnScreens": ["home", "search"],
+        "waitingTimeToDisplay": 2
+      },
+      {
+        "type": "banner",
+        "iosId": "ca-app-pub-2101182411274198/5418791562",
+        "provider": "google",
+        "androidId": "ca-app-pub-2101182411274198/4052745095"
+      },
+      {
+        "type": "interstitial",
+        "iosId": "ca-app-pub-3940256099942544/4411468910",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/4411468910",
+        "showOnScreens": ["profile"],
+        "waitingTimeToDisplay": 5
+      },
+      {
+        "type": "reward",
+        "iosId": "ca-app-pub-3940256099942544/1712485313",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/4411468910",
+        "showOnScreens": ["cart"]
+      },
+      {
+        "type": "banner",
+        "iosId": "IMG_16_9_APP_INSTALL#430258564493822_876131259906548",
+        "provider": "facebook",
+        "androidId": "IMG_16_9_APP_INSTALL#430258564493822_489007588618919",
+        "showOnScreens": ["home"]
+      },
+      {
+        "type": "interstitial",
+        "iosId": "430258564493822_489092398610438",
+        "provider": "facebook",
+        "androidId": "IMG_16_9_APP_INSTALL#430258564493822_489092398610438"
+      }
+    ],
+    "enable": false,
+    "adMobAppIdIos": "ca-app-pub-7432665165146018~2664444130",
+    "googleTestingId": [],
+    "adMobAppIdAndroid": "ca-app-pub-7432665165146018~2664444130",
+    "facebookTestingId": ""
+  },
+  "firebaseDynamicLinkConfig": {
+    "link": "https://mstore.io/",
+    "isEnabled": true,
+    "uriPrefix": "",
+    "iOSBundleId": "com.inspireui.mstore.flutter",
+    "iOSAppStoreId": "1469772800",
+    "androidPackageName": "",
+    "androidPackageName": "",
+    "iOSAppMinimumVersion": "1.0.1",
+    "shortDynamicLinkEnable": true,
+    "androidAppMinimumVersion": 1
+  },
+  "languagesInfo": [
+    {
+      "code": "en",
+      "icon": "assets/images/country/gb.png",
+      "name": "English",
+      "text": "English",
+      "storeViewCode": ""
+    },
+    {
+      "code": "ar",
+      "icon": "assets/images/country/ar.png",
+      "name": "Arabic",
+      "text": "العربية",
+      "storeViewCode": "ar"
+    }
+  ],
+  "paymentConfig": {
+    "SmartCOD": {"enabled": true, "extraFee": 10, "amountStop": 200},
+    "EnableReview": true,
+    "RefundPeriod": 7,
+    "EnableAddress": true,
+    "GuestCheckout": true,
+    "webPaymentIds": ["cod", "bacs"],
+    "EnableShipping": true,
+    "ShowOrderNotes": true,
+    "CheckoutPageSlug": {"en": "checkout"},
+    "EnableCreditCard": false,
+    "UpdateOrderStatus": false,
+    "EnableCustomerNote": true,
+    "EnableRefundCancel": true,
+    "excludedPaymentIds": [],
+    "DefaultStateISOCode": "LA",
+    "DefaultCountryISOCode": "US",
+    "EnableOnePageCheckout": true,
+    "NativeOnePageCheckout": false,
+    "allowSearchingAddress": true,
+    "ShowTransactionDetails": true,
+    "EnableAddressLocationNote": false,
+    "EnableAlphanumericZipCode": false,
+    "PaymentListAllowsCancelAndRefund": [],
+    "ShowWebviewCheckoutSuccessScreen": true
+  },
+  "payments": {
+    "tap": "assets/icons/payment/tap.png",
+    "paypal": "assets/icons/payment/paypal.svg",
+    "stripe": "assets/icons/payment/stripe.svg",
+    "midtrans": "assets/icons/payment/midtrans.png",
+    "paystack": "assets/icons/payment/paystack.png",
+    "razorpay": "assets/icons/payment/razorpay.svg",
+    "xendit_cc": "assets/icons/payment/xendit.png",
+    "thawani_gw": "assets/icons/payment/thawani.png",
+    "ppcp-gateway": "assets/icons/payment/paypal.svg",
+    "myfatoorah_v2": "assets/icons/payment/myfatoorah.png",
+    "stripe_v2_apple_pay": "assets/icons/payment/apple-pay-mark.svg",
+    "thai-promptpay-easy": "assets/icons/payment/prompt-pay.png",
+    "expresspay_apple_pay": "assets/icons/payment/apple-pay-mark.svg",
+    "stripe_v2_google_pay": "assets/icons/payment/google-pay-mark.png"
+  },
+  "paypalConfig": {
+    "secret":
+        "ECbFREri7NFj64FI_9WzS6A0Az2DqNLrVokBo0ZBu4enHZKMKOvX45v9Y1NBPKFr6QJv2KaSp5vk5A1G",
+    "enabled": false,
+    "clientId":
+        "ASlpjFreiGp3gggRKo6YzXMyGM6-NwndBAQ707k6z3-WkSSMTPDfEFmNmky6dBX00lik8wKdToWiJj5w",
+    "returnUrl": "",
+    "nativeMode": false,
+    "production": false,
+    "paymentMethodId": "paypal"
+  },
+  "paypalExpressConfig": {
+    "enabled": false,
+    "password": "9MN73T4JHTBDY5W7",
+    "username": "sb-wea3q30917031_api1.business.example.com",
+    "signature": "A-X91d6dvj07IIDTUn5hM8p8w8LxA-5D.cnvNUgufzpxxf1NNZBYh3kq",
+    "production": false,
+    "paymentAction": "Sale",
+    "paymentMethodId": "paypal_express"
+  },
+  "razorpayConfig": {
+    "keyId": "rzp_test_SDo2WKBNQXDk5Y",
+    "enabled": false,
+    "keySecret": "RrgfT3oxbJdaeHSzvuzaJRZf",
+    "paymentMethodId": "razorpay"
+  },
+  "mercadoPagoConfig": {
+    "enabled": false,
+    "production": false,
+    "accessToken":
+        "TEST-5726912977510261-102413-65873095dc5b0a877969b7f6ffcceee4-613803978",
+    "paymentMethodId": "woo-mercado-pago-basic"
+  },
+  "payTmConfig": {
+    "enabled": false,
+    "merchantId": "your-merchant-id",
+    "production": false,
+    "paymentMethodId": "paytm"
+  },
+  "payStackConfig": {
+    "enabled": false,
+    "publicKey": "pk_test_a1a37615c9ca90dead5dd84dedbb5e476b640a6f",
+    "secretKey": "sk_test_d833fcaa6c02a61a9431d2026046c0517888a4a7",
+    "production": false,
+    "paymentMethodId": "paystack",
+    "enableMobileMoney": true,
+    "supportedCurrencies": ["ZAR"]
+  },
+  "flutterwaveConfig": {
+    "enabled": false,
+    "publicKey": "FLWPUBK_TEST-72b90e0734da8c9e43916adf63cd711e-X",
+    "production": false,
+    "paymentMethodId": "rave"
+  },
+  "inAppPurchaseConfig": {
+    "enabled": false,
+    "consumableProductIDs": ["c"],
+    "subscriptionProductIDs": ["c"],
+    "nonConsumableProductIDs": []
+  },
+  "expressPayConfig": {
+    "enabled": true,
+    "merchantId": "",
+    "production": false,
+    "merchantKey": "b2be2ffc-c8b9-11ed-82a9-42eb4e39c8ae",
+    "paymentMethodId": "shahbandrpay",
+    "merchantPassword": "4a00a5fd3c63dd2b743c75746af6ffe2"
+  },
+  "thaiPromptPayConfig": {
+    "enabled": false,
+    "paymentMethodId": "thai-promptpay-easy"
+  },
+  "fibConfig": {
+    "enabled": false,
+    "clientId": "narin-beauty",
+    "clientSecret": "7ffcd642-87b7-4cc0-b75d-c25d5276cffe",
+    "paymentMethodId": "fib"
+  },
+  "thawaniConfig": {
+    "enabled": true,
+    "secretKey": "rRQ26GcsZzoEhbrP2HZvLYDbn9C9et",
+    "production": false,
+    "publishableKey": "HGvTMLDssJghr9tlN9gr4DVYt0qyBy",
+    "paymentMethodId": "thawani_gw"
+  },
+  "defaultCountryShipping": [
+    {"name": "Egypt", "iosCode": "EG", "emoji": "🇪🇬"}
+  ],
+  "afterShip": {
+    "api": "e2e9bae8-ee39-46a9-a084-781d0139274f",
+    "tracking_url": ""
+  },
+  "googleApiKey": {
+    "ios": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg",
+    "web": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg",
+    "android": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg"
+  },
+  "productCard": {"defaultImage": "assets/images/no_product_image.png"},
+  "productDetail": {
+    "height": 0.6,
+    "layout": "flatStyle",
+    "showSku": true,
+    "safeArea": false,
+    "marginTop": 0,
+    "showBrand": true,
+    "showVideo": true,
+    "expandTags": true,
+    "showRating": true,
+    "borderRadius": 3,
+    "enableReview": true,
+    "expandBrands": true,
+    "expandInfors": true,
+    "expandReviews": true,
+    "buyButtonStyle": "normal",
+    "attributeLayout": "inline",
+    "autoPlayGallery": false,
+    "expandSizeGuide": true,
+    "limitDayBooking": 14,
+    "showProductTags": true,
+    "ShowImageGallery": true,
+    "expandCategories": true,
+    "expandDescription": true,
+    "showRecentProduct": true,
+    "showStockQuantity": true,
+    "productImageLayout": "page",
+    "showQuantityInList": false,
+    "showRelatedProduct": true,
+    "SliderIndicatorType": "number",
+    "alwaysShowBuyButton": true,
+    "attributeImagesSize": 50,
+    "ForceWhiteBackground": false,
+    "showThumbnailAtLeast": 1,
+    "hideInvalidAttributes": false,
+    "productListItemHeight": 125,
+    "showProductCategories": true,
+    "SliderShowGoBackButton": true,
+    "fixedBuyButtonToBottom": false,
+    "AutoSelectFirstAttribute": true,
+    "ShowSelectedImageVariant": true,
+    "showAddToCartInSearchResult": true
+  },
+  "blogDetail": {
+    "showHeart": true,
+    "showComment": true,
+    "showSharing": true,
+    "showAuthorInfo": true,
+    "showRelatedBlog": true,
+    "enableAudioSupport": false,
+    "showTextAdjustment": true
+  },
+  "productVariantLayout": {
+    "size": "box",
+    "color": "color",
+    "height": "option",
+    "color-image": "image"
+  },
+  "productAddons": {
+    "allowMultiple": false,
+    "allowImageType": true,
+    "allowVideoType": true,
+    "allowCustomType": true,
+    "allowedCustomType": ["png", "pdf", "docx"],
+    "fileUploadSizeLimit": 5
+  },
+  "cartDetail": {
+    "style": "style01",
+    "maxAllowQuantity": 10,
+    "minAllowTotalCartValue": 0
+  },
+  "productVariantLanguage": {
+    "ar": {
+      "size": "بحجم",
+      "color": "اللون",
+      "height": "ارتفاع",
+      "color-image": "اللون"
+    },
+    "en": {
+      "size": "Size",
+      "color": "Color",
+      "height": "Height",
+      "color-image": "Color"
+    },
+    "vi": {
+      "size": "Kích thước",
+      "color": "Màu",
+      "height": "Chiều Cao",
+      "color-image": "Màu"
+    }
+  },
+  "excludedCategory": "311",
+  "saleOffProduct": {
+    "Color": "#C7222B",
+    "ShowCountDown": true,
+    "HideEmptySaleOffLayout": false
+  },
+  "notStrictVisibleVariant": true,
+  "configChat": {
+    "EnableSmartChat": true,
+    "enableVendorChat": true,
+    "UseRealtimeChat": false,
+    "showOnScreens": ["profile"],
+    "hideOnScreens": [],
+    "version": "2"
+  },
+  "smartChat": [
+    {
+      "app": "chatGPT",
+      "imageData": "https://i.imgur.com/pp1qlPd.png",
+      "description": "Chat GPT"
+    },
+    {
+      "app": "https://wa.me/+201222295026",
+      "iconData": "whatsapp",
+      "description": "WhatsApp"
+    },
+    {"app": "tel:01222295026", "iconData": "phone", "description": "Call Us"},
+    {
+      "app": "https://www.facebook.com/toysbox.eg?mibextid=LQQJ4d",
+      "iconData": "facebookMessenger",
+      "description": "Facebook "
+    }
+  ],
+
+  "loadingIcon": {"layout": "spinkit", "type": "fadingCube", "size": 30},
+  "splashScreen": {
+    "type": "fade-in",
+    "image": null,
+    "boxFit": "contain",
+    "enable": true,
+    "duration": 2000,
+    "paddingTop": 0,
+    "paddingLeft": 0,
+    "paddingRight": 0,
+    "animationName": "Toys Box",
+    "paddingBottom": 0,
+    "backgroundColor": "#ffffff"
+  },
+  "orderConfig": {"version": 1},
+  "lightConfig": {
+    "MainColor": "8dff8801",
+    "logo":
+        "https://toysbox.store/wp-content/uploads/2024/09/cropped-toylogo-300x300.png",
+    "saleColor": "#E15241"
+  },
+  "darkConfig": {
+    "MainColor": "ffff8801",
+    "logo":
+        "https://toysbox.store/wp-content/uploads/2024/09/cropped-toylogo-300x300.png",
+    "saleColor": "#E15241"
+  }
+};
